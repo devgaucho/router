c8a97f7cb487233f2aae17b63734472d381456f7
gaucho.dev <119246170+devgaucho@users.noreply.github.com>
2023-03-02 10:32:26 -0300

    Função "segment" adicionada

--- src/FireRouter.php
+++ src/FireRouter.php
@@ -2,43 +2,52 @@
 
 use gaucho\Router;
 
-function get(...$params)
-{
-    Router::get(...$params);
-}
-function post(...$params)
-{
-    Router::post(...$params);
-}
-function put(...$params)
-{
-    Router::put(...$params);
-}
-function patch(...$params)
-{
-    Router::patch(...$params);
-}
-function delete(...$params)
-{
+function delete(...$params){
     Router::delete(...$params);
 }
-function options(...$params)
-{
-    Router::options(...$params);
-}
-function headMethod(...$params)
-{
-    Router::head(...$params);
-}
-
-function dispatch()
-{
+function dispatch(){
 	Router::dispatch();
 }
-
-function view($name,$data=false)
-{
-    return new View($name,$data);
+function get(...$params){
+    Router::get(...$params);
 }
-
+function headMethod(...$params){
+    Router::head(...$params);
+}
+function options(...$params){
+    Router::options(...$params);
+}
+function patch(...$params){
+    Router::patch(...$params);
+}
+function post(...$params){
+    Router::post(...$params);
+}
+function put(...$params){
+    Router::put(...$params);
+}
+function segment($segmentId=null){
+    $str=$_SERVER["REQUEST_URI"];
+    $str=@explode('?', $str)[0];
+    $arr=explode('/', $str);
+    $arr=array_filter($arr);
+    $arr=array_values($arr);
+    if(count($arr)<1){
+        $segment[1]='/';
+    }else{
+        $i=1;
+        foreach ($arr as $key => $value) {
+            $segment[$i++]=$value;
+        }
+    }
+    if(is_null($segmentId)){
+        return $segment;
+    }else{
+        if(isset($segment[$segmentId])){
+            return $segment[$segmentId];
+        }else{
+            return false;
+        }
+    }
+}	
 register_shutdown_function('dispatch');
