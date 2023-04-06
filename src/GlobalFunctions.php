ce3c1dee4b7bdcb750205b0d764c32043381fe6b
devgaucho <119246170+devgaucho@users.noreply.github.com>
2023-04-06 11:54:02 -0300

    asset(), plugins(), start_time() e end_time()

--- src/GlobalFunctions.php
+++ src/GlobalFunctions.php
@@ -2,6 +2,37 @@
 
 use gaucho\Router;
 
+function asset($urls,$autoIndent=true){
+    if(is_string($urls)){
+        $arr[]=$urls;
+        $urls=$arr;
+    }
+    foreach($urls as $key=>$url){
+        $filename=__DIR__.'/../../../../'.$url;
+        $path_parts=pathinfo($url);
+        $ext=$path_parts['extension'];
+        if(file_exists($filename)){
+            $md5=md5_file($filename);
+            if(isset($_ENV['SITE_URL'])){
+                $url=SITE_URL.'/'.$url."?$md5";
+            }else{
+                $url=$url."?$md5";
+            }
+            if($autoIndent and $key<>0){
+                print '    ';
+            }
+            if($ext=='css'){
+                print '<link rel="stylesheet" href="'.$url.'" />';
+            }
+            if($ext=='js'){
+                $js_str='<script type="text/javascript" src="';
+                $js_str.=$url.'"></script>';
+                print $js_str;
+            }
+            print PHP_EOL;
+        }
+    }
+}
 function code($httpCode){
 	http_response_code($httpCode);
 }
@@ -9,9 +40,18 @@ function delete(...$params){
     Router::delete(...$params);
 }
 function dispatch(){
-	if(!isCli()){
-		Router::dispatch();
-	}
+    plugins();
+    showErrors();
+    if(!isCli()){
+      Router::dispatch();
+  }
+}
+function end_time($start_str){
+    $end_str=microtime(1);
+    if(!function_exists('bcdiv')){
+        die("sudo apt install php-bcmath -y && sudo systemctl restart apache2");
+    }
+    return bcsub($end_str,$start_str,3);//tempo em segundos
 }
 function get(...$params){
     Router::get(...$params);
@@ -29,11 +69,11 @@ function isCli(){
 function json($data,$print=true){
 	$str=json_encode($data,JSON_PRETTY_PRINT);
 	if($print){
-	    header('Content-Type: application/json');
-	    die($str);
-	}else{
-		return $str;
-	}
+       header('Content-Type: application/json');
+       die($str);
+   }else{
+      return $str;
+  }
 }
 function mustache($templateName,$data='',$print=true){
 	$str=__DIR__.'/../../../../view/'.$templateName.'.html';
@@ -56,6 +96,14 @@ function options(...$params){
 }
 function patch(...$params){
     Router::patch(...$params);
+}
+function plugins(){
+    $plugins_path_str=__DIR__.'/../../../../plugin';
+    if(file_exists($plugins_path_str)){
+        foreach (glob($plugins_path_str.'/*.php') as $filename){
+            require_once $filename;
+        }
+    }
 }
 function post(...$params){
     Router::post(...$params);
@@ -87,8 +135,12 @@ function segment($segmentId=null){
         }
     }
 }	
-function showErrors($bool=true){
-    if($bool){
+function showErrors($display_errors=true){
+    $display_errors=
+    if(isset($_ENV['DISPLAY_ERRORS'])){
+        $display_errors=$_ENV['DISPLAY_ERRORS'];
+    }
+    if($display_errors){
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
@@ -97,6 +149,9 @@ function showErrors($bool=true){
         ini_set('display_startup_errors', 0);
         error_reporting(0);
     }
+}
+function start_time(){
+    return microtime(1);
 }
 function view($name,$data='',$print=true){
 	return mustache($name,$data,$print);
